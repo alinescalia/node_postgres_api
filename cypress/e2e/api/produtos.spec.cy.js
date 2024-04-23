ee1436fc387021c45b3892458893eb88aee41843
aline <liscalia@gmail.com>
2024-04-22 21:51:02 -0300

    configurando cypress

--- cypress/e2e/api/produtos.spec.cy.js
+++ cypress/e2e/api/produtos.spec.cy.js
@@ -2,7 +2,7 @@
 
 describe('GET de todos os produtos', () => {
     it('GET/produtos', () => {
-        cy.request('http://127.0.0.1:5005/produtos')
+        cy.request('/produtos')
             .its('status')
             .should('equal', 200)
 
@@ -12,7 +12,7 @@ describe('GET de todos os produtos', () => {
 
 describe('GET de produtos especificos', () => {
     it('GET/produto/id', () => {
-        cy.request('http://127.0.0.1:5005/produtos/3')
+        cy.request('/produtos/3')
             .its('status')
             .should('equal', 200)
     })
